0f87e6eac1dbf985494b291e10c31b0964df8602
MR777 <username@users.noreply.github.com>
2016-11-11 03:53:53 +0430

    test init

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -38,15 +38,11 @@ func main() {
 }
 
 // Init resets all the things
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-    if len(args) != 1 {
-        return nil, errors.New("Incorrect number of arguments. Expecting 1")
-    }
+func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+  if len(args) != 1 {
+       return nil, errors.New("Incorrect number of arguments. Expecting 1")
+  }
 
-    err := stub.PutState("hello_world", []byte(args[0]))
-    if err != nil {
-        return nil, err
-    }
 
     return nil, nil
 }
